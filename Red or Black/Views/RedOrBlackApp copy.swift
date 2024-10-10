11bea8507de0fec0ec6d3e39e4b2fa9b420f962b
Nick Shahbaz <157538526+BoomBoom135@users.noreply.github.com>
2024-10-10 18:52:44 -0400

    Orginized Files

---
+++ Red or Black/Views/RedOrBlackApp copy.swift
@@ -0,0 +1,18 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Nick Shahbaz on 2024-06-05.
+//
+
+import SwiftUI
+
+@main
+struct RedOrBlack: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
